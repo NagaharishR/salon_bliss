a2666fca291a8363419812309f8b814a3da82021
New <rnagaharish@gmail.com>
2023-11-23 14:37:27 +0530

    this commit contains more data

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,6 +15,7 @@ class HomePage extends StatelessWidget {
             height: 300,
             width: 200,),
             SizedBox(height: 8),
+
             Row(
               children: [
               Padding(
@@ -61,7 +62,7 @@ class HomePage extends StatelessWidget {
             SizedBox(height: 10),
             Row(
               children: [
-                Padding(padding: EdgeInsets.symmetric(horizontal: 45)),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 28)),
                 ElevatedButton(
                     style: TextButton.styleFrom(
                       minimumSize: Size(300, 60),
@@ -88,12 +89,118 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
-            Container(
-              width: double.infinity,
-              height: 500,
-              color: Colors.white10,
-
-            )
+             SizedBox(height: 8,),
+             Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 15)),
+                Icon(Icons.call,
+                color: Colors.redAccent,),
+                Expanded(
+                  child: Text(
+                    'Call us                                                                                                     020 7100 9290',
+                  ),
+                ),
+              ],
+            ),
+            Divider( indent: 15,
+              endIndent: 15,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 15)),
+                Icon(Icons.location_on_outlined,
+                color: Colors.redAccent,),
+                Expanded(
+                  child: Text(
+                    'Find us                                                                                                       London, England,United kingdom(mainland)',
+                  ),
+                ),
+              ],
+            ),
+            Divider( indent: 15,
+              endIndent: 15,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 15)),
+                Icon(Icons.mail_outline_rounded,
+                  color: Colors.redAccent,),
+                SizedBox(width: 10),
+                Text('Email us'),
+              ],
+            ),
+            Divider( indent: 15,
+              endIndent: 15,),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+              Text('Opening hours',
+              style: TextStyle(
+                color: Colors.redAccent
+              ),)
+            ],
+            ),
+            SizedBox(height: 9),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Sunday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 100)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Monday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 98)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Tuesday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 95)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Wednesday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 85)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Thursday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 92)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Friday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 102)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.symmetric(horizontal: 13)),
+                Text('Saturday'),
+                Padding(padding: EdgeInsets.symmetric(horizontal: 93)),
+                Text('09:00 - 19:00'),
+              ],
+            ),
 
           ],
         ),
